0f4dae874dee4390d5769dbbe1d87d0d910ec848
kuzmichalexey <44007284+kuzmichalexey@users.noreply.github.com>
2018-12-19 00:39:41 +0300

    Update TrigonometryLibrary.h

--- C++ library catalog/TrigonometryLibrary.h
+++ C++ library catalog/TrigonometryLibrary.h
@@ -7,6 +7,10 @@ using namespace std;
 
 double Sin(double value, double accurancy, int& count);
 
+bool PointOfCircle(double x0, double y0, double x, double y, double radious);
+
+bool PointOfCircleSector(double xLowerBound, double xUpperBound, double yLowerBound, double yUpperBound, double, double, double, double, double);
+
 
 double Sin(double x, double accurancy, int& n)
 {
@@ -26,5 +30,27 @@ double Sin(double x, double accurancy, int& n)
 	n = i;
 
 	return sinus;
+}
+
+
+bool PointOfCircle(double x0, double y0, double x, double y, double radious)
+{
+	if (((x - x0)*(x - x0) - (y - y0)*(y - y0)) <= radious * radious)
+		return 1;
+
+	return 0;
+}
+
+bool PointOfCircleSector(double xLowerBound, double xUpperBound, double yLowerBound, double yUpperBound, double x0, double y0, double x, double y, double radious)
+{
+	if ((xLowerBound <= x && x >= xUpperBound) && (y >= yLowerBound && y <= yUpperBound))
+	{
+		if (PointOfCircle(x0, y0, x, y, radious))
+		{
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
